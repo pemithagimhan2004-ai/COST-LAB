c768a535963e964d5f735127824b3d7dfcc98146
pemithagimhan2004-ai <pemithagimhan2004@gmail.com>
2026-05-25 13:32:58 +0530

    create folder

---
+++ cost_lab_sheet/src/main/java/com/mycompany/cost_lab_sheet/Cost_lab_sheet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cost_lab_sheet;
+
+/**
+ *
+ * @author pemit
+ */
+public class Cost_lab_sheet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
